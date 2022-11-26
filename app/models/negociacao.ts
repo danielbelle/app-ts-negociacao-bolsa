b6d9d10b641ea73001164d4a624b83e3876b07bd
Daniel Henrique Bellé <47600437+danielbelle@users.noreply.github.com>
2022-11-26 14:23:02 -0300

    3rd commit

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,29 +1,18 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(
+        private _data: Date,
+        public readonly quantidade: number,
+        public readonly valor: number
+    ) { }
+
+    get volume(): number {
+        return this.quantidade * this.valor;
     }
 
-    get data() {
+    get data(): Date {
+        const data = new Date(this._data.getTime());
         return this._data;
-    }
-
-
-    get quantidade() {
-        return this._quantidade;
-    }
-
-    get valor() {
-        return this._valor;
-    }
-
-    get volume() {
-        return this._quantidade * this._valor;
     }
 
 }
